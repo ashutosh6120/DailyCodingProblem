b4d941950511991cb2535fe8baf0248bde72f716
ashutosh6120 <ashutosh4064@gmail.com>
2023-03-10 23:52:22 +0530

    prbm 13 soln added

---
+++ 013-Amazon/Problem13.java
@@ -0,0 +1,49 @@
+/*************************************************************************************************************************************
+This problem was asked by Amazon.
+Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+**************************************************************************************************************************************/
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Oleg Cherednik
+ * @since 27.01.2019
+ */
+public class Problem13 {
+
+    public static void main(String... args) {
+        System.out.println(findLongestSubstring("abcba", 2)); // bcb
+    }
+
+    public static String findLongestSubstring(String str, int k) {
+        Map<Character, Set<Integer>> map = new HashMap<>();
+        String res = null;
+
+        for (int i = 0, j = 0; j < str.length(); ) {
+            if (map.containsKey(str.charAt(j)))
+                map.get(str.charAt(j)).add(j++);
+            else if (map.size() < k) {
+                map.put(str.charAt(j), new HashSet<>());
+                map.get(str.charAt(j)).add(j++);
+            } else {
+                if (res == null || j - i + 1 > res.length())
+                    res = str.substring(i, j);
+
+                map.get(str.charAt(i)).remove(i);
+
+                if (map.get(str.charAt(i)).isEmpty())
+                    map.remove(str.charAt(i));
+
+                i++;
+            }
+        }
+
+        return res;
+    }
+
+}
